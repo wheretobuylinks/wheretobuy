b70e63d65c34ccaad2cc2088ac49a2696376d2fd
wheretobuylinks <trendinginaliexpress@gmail.com>
2026-06-06 16:47:38 -0300

    Update config.ts

--- tina/config.ts
+++ tina/config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tinacms";
 
 export default defineConfig({
-  branch: "main", 
+  branch: "cloudflare/workers-autoconfig", 
   // Busca el Client ID de todas las formas posibles según el framework que uses
   clientId: "53a6b864-54f0-4d88-8b65-06b0691457d2",
   token: process.env.TINA_TOKEN,
